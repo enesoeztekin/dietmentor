c8d08f0ac4e3f4640a1267159b43f979825ce6e8
Enes Öztekin <enesoztekinofficial@gmail.com>
2025-05-02 17:12:45 +0300

    diet generator bux fix global cfg

--- local/dietgenerator/index.php
+++ local/dietgenerator/index.php
@@ -2,7 +2,7 @@
 
 global $CFG;
 require('../../config.php');
-require_once('forms/userinfo_form.php');
+require_once($CFG->dirroot . '/local/dietgenerator/forms/userinfo_form.php');
 require_once($CFG->dirroot . '/local/dietgenerator/classes/api/OpenAIClient.php');
 require_once($CFG->dirroot . '/local/dietgenerator/classes/logic/DietCourseBuilder.php');
 
@@ -13,26 +13,37 @@ $PAGE->set_url(new moodle_url('/local/dietgenerator/index.php'));
 $PAGE->set_title('Diyet Programı Oluşturucu');
 $PAGE->set_heading('Diyet Programı Oluşturucu');
 
+// Header ve başlık yazdırma
 echo $OUTPUT->header();
 echo $OUTPUT->heading('Diyet Programı Oluşturucu');
 
+// Formu oluşturma ve işleme
 $form = new userinfo_form();
 
 if ($form->is_cancelled()) {
+    // Form iptal edilirse anasayfaya yönlendir
     redirect(new moodle_url('/'));
 } else if ($data = $form->get_data()) {
+    // Formdan gelen veriler alındı
+    // OpenAI API istemcisini oluştur
     $client = new \local_dietgenerator\api\OpenAIClient();
+
+    // Kullanıcıdan alınan verileri kullanarak bir prompt oluştur
     $prompt = "Kullanıcının yaşı: {$data->age}, cinsiyeti: {$data->gender}, boyu: {$data->height} cm, kilosu: {$data->weight} kg. Bu bilgilere göre 7 günlük bir diyet listesi hazırla. Her gün için sabah kahvaltısı, öğle ve akşam yemeği olacak şekilde belirt.";
 
+    // OpenAI API'sine istek gönder
     $response = $client->generate($prompt);
 
-    // Şimdilik cevabı yazdıralım
+    // Şimdilik cevabı ekrana yazdırıyoruz
     echo html_writer::div('<strong>OpenAI Yanıtı:</strong><br>' . nl2br($response), 'box generalbox');
 
-    // İleride course builder'a aktaracağız
+    // Burada kurs oluşturma kısmını ileride ekleyeceğiz
+    // DietCourseBuilder ile alınan yanıtı kursa aktarabilirsiniz
 
 } else {
+    // Formu göster
     $form->display();
 }
 
+// Footer'ı yazdır
 echo $OUTPUT->footer();
