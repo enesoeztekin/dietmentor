e96a0e93c63ad824898a5e472c5b77066d9b2376
Enes Öztekin <enesoztekinofficial@gmail.com>
2025-05-20 17:23:56 +0300

    + besin kaynakları

--- local/dietgenerator/forms/userinfo_form.php
+++ local/dietgenerator/forms/userinfo_form.php
@@ -25,6 +25,39 @@ class userinfo_form extends moodleform {
         $mform->addElement('select', 'gender', 'Cinsiyet', $genderoptions);
         $mform->addRule('gender', 'Gerekli', 'required');
 
+        // Protein kaynakları
+        $proteinoptions = [
+            'tavuk' => 'Tavuk',
+            'balik' => 'Balık',
+            'yumurta' => 'Yumurta',
+            'kirmizi_et' => 'Kırmızı Et',
+            'peynir' => 'Peynir'
+        ];
+        $mform->addElement('select', 'protein_sources', 'Protein Kaynakları', $proteinoptions, ['multiple' => 'multiple']);
+        $mform->addRule('protein_sources', 'En az bir protein kaynağı seçiniz.', 'required');
+
+        // Karbonhidrat kaynakları
+        $carboptions = [
+            'pirinc' => 'Pirinç',
+            'bulgur' => 'Bulgur',
+            'patates' => 'Patates',
+            'yulaf' => 'Yulaf',
+            'makarna' => 'Makarna'
+        ];
+        $mform->addElement('select', 'carb_sources', 'Karbonhidrat Kaynakları', $carboptions, ['multiple' => 'multiple']);
+        $mform->addRule('carb_sources', 'En az bir karbonhidrat kaynağı seçiniz.', 'required');
+
+        // Yağ kaynakları
+        $fatoptions = [
+            'zeytinyagi' => 'Zeytinyağı',
+            'avokado' => 'Avokado',
+            'ceviz' => 'Ceviz',
+            'fistik' => 'Fıstık',
+            'badem' => 'Badem'
+        ];
+        $mform->addElement('select', 'fat_sources', 'Yağ Kaynakları', $fatoptions, ['multiple' => 'multiple']);
+        $mform->addRule('fat_sources', 'En az bir yağ kaynağı seçiniz.', 'required');
+
         $mform->addElement('submit', 'submitbutton', 'Diyet Listesi Oluştur');
     }
 }
